56694541ff02047a1b71c7cb6623aaefd7e9e64e
Tactfulgalaxy <adebisiibrahim909@gmail.com>
2025-02-23 04:31:57 +0100

    Initial commit with Merkle Airdrop contract

---
+++ script/Interact.s.sol
@@ -0,0 +1,38 @@
+// SPDX-LICENSE-Identifier: MIT
+
+pragma solidity ^0.8.18;
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+
+contract ClaimAirdrop is Script{
+    address CLAIMING_ADDRESS = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    uint256 CLAIMING_AMOUNT = 25 * 1e18;
+    bytes32 PROOF_ONE = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
+    bytes32 PROOF_TWO = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] proof = [PROOF_ONE,PROOF_TWO];
+    bytes private SIGNATURE = hex"b8823364c90ea0d2700d5ad0fe39d16778bc07ce7df4779ff35e4b2660d043cb74a002439225d1d518f9f1cf3db005f5e143196543fd5146a34bf63f0b810ade1b";
+
+    error _ClaimAirdropScript_InvalidSignatureLength();
+    function claimAirdrop(address airdrop) public {
+        vm.startBroadcast();
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(SIGNATURE);
+        MerkleAirdrop(airdrop).claim(CLAIMING_ADDRESS, CLAIMING_AMOUNT, proof, v, r, s);
+        vm.stopBroadcast();
+    }
+    function splitSignature(bytes memory sig) public pure  returns (uint8 v, bytes32 r, bytes32 s){
+        if (sig.length != 65){
+            revert _ClaimAirdropScript_InvalidSignatureLength();
+        }
+        assembly{
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+    }
+
+    function run() external{
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("MerkleAirdrop", block.chainid);
+        ClaimAirdrop(mostRecentlyDeployed);
+    }
+}
---
+++ test/MerkleAirdrop.t.sol
@@ -0,0 +1,63 @@
+// SPDX-LICENSE-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+
+import {Test, console} from "forge-std/Test.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {BagelToken} from "../src/BagelToken.sol";
+import {DeployMerkleAirdrp} from "script/DeployMerkleAirdrop.s.sol";
+
+contract MerkleAirdropTest is Test{
+    MerkleAirdrop public airdrop;
+    BagelToken public token;
+
+    bytes32 public ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    address user;
+    address gasPayer;
+    uint256 userPrivKey;
+    uint256 public AMOUNT_TO_CLAIM = 25 * 1e18;
+    uint256 public AMOUNT_TO_SEND = AMOUNT_TO_CLAIM * 4;
+    bytes32 proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] public PROOF = [proofOne, proofTwo];
+
+    function setUp() public{
+        token = new BagelToken();
+        airdrop = new MerkleAirdrop(ROOT, token);
+        token.mint(token.owner(), AMOUNT_TO_SEND);
+        token.transfer(address(airdrop), AMOUNT_TO_SEND);
+        (user, userPrivKey) = makeAddrAndKey("user");
+        gasPayer = makeAddr("gasPayer");
+    }
+    function testUsersCanClaim() public{
+        uint256 startingBalance = token.balanceOf(user);
+        bytes32 digest = airdrop.getMessageHash(user, AMOUNT_TO_CLAIM);
+
+        //signs a message
+        
+        (uint8 v, bytes32 r, bytes32  s) = vm.sign(userPrivKey, digest);
+        
+
+        // gasPayer CALLS CLAIM USING THE SIGNED MESSAGE
+        vm.prank(gasPayer);
+        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF,v ,r ,s);
+        
+        uint256 endingBalance = token.balanceOf(user);
+        console.log("Ending Balance: ", endingBalance);
+        assertEq(endingBalance - startingBalance, AMOUNT_TO_CLAIM);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
